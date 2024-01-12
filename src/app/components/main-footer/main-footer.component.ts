d8114d306dc4d59e98d8deb47ea227294d4de493
Artem <artem.egorov.art2006@gmail.com>
2024-01-12 23:06:55 +0300

    Настроил footer

--- src/app/components/main-footer/main-footer.component.ts
+++ src/app/components/main-footer/main-footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CircleLinkComponent } from '../UI/link/circle-link/circle-link.component';
+import { LinkComponent } from '../UI/link/link.component';
 
 @Component({
   selector: 'app-main-footer',
   standalone: true,
-  imports: [CircleLinkComponent],
+  imports: [CircleLinkComponent, LinkComponent],
   templateUrl: './main-footer.component.html',
   styleUrl: './main-footer.component.scss'
 })
