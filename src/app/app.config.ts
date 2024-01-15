59c5b1a6da4ae15eb41c980b018cd44d2d712c20
Artem <artem.egorov.art2006@gmail.com>
2024-01-15 23:29:45 +0300

    Добавил Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,14 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthorizationInterceptorService } from './services/interceptors/authorization-interceptor.service';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
     importProvidersFrom(HttpClientModule),
     {
-      provide: HTTP_INTERCEPTORS,
-      multi: true,
-      useExisting: AuthorizationInterceptorService
-    }
-  ]
+        provide: HTTP_INTERCEPTORS,
+        multi: true,
+        useExisting: AuthorizationInterceptorService
+    }, provideAnimations()]
 };
