125c5c90866372570ef1c1e3c435dbae0d9a78ad
Artem <artem.egorov.art2006@gmail.com>
2024-01-18 23:25:34 +0300

    Анализ и косметические правки

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { UserCardComponent } from '../../components/user-card/user-card.component';
+import { ProfileEditFormComponent } from '../../components/form/profile-edit-form/profile-edit-form.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [UserCardComponent, ProfileEditFormComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
-
+  value = "Test"
 }
