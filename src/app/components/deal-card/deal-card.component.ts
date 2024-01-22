820875410ebddef9a21c53759f611a9a76003d63
Artem <artem.egorov.art2006@gmail.com>
2024-01-22 13:00:29 +0300

    Отображение Категорий при просмотре Заказов

--- src/app/components/deal-card/deal-card.component.ts
+++ src/app/components/deal-card/deal-card.component.ts
@@ -2,11 +2,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { DealCard } from './../../models/deal';
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatChipsModule } from '@angular/material/chips';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-deal-card',
   standalone: true,
-  imports: [MatButtonModule, RouterLink],
+  imports: [MatButtonModule, RouterLink, MatChipsModule, NgFor],
   templateUrl: './deal-card.component.html',
   styleUrl: './deal-card.component.scss'
 })
