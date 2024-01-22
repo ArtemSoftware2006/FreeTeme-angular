ab42d819e76f0301a18e167a3ae4c7c8d9d64b5e
Artem <artem.egorov.art2006@gmail.com>
2024-01-22 13:01:29 +0300

    Добавил в профиль карточку Пользователя и изменение профиля

--- src/app/components/form/profile-edit-form/profile-edit-form.component.ts
+++ src/app/components/form/profile-edit-form/profile-edit-form.component.ts
@@ -1,37 +1,89 @@
+import { User, UserUpdateProfile, UserUpdateProfileRequest } from './../../../models/user';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UserProfile } from '../../../models/user';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormInputComponent } from '../../UI/input/form-input/form-input.component';
 import { MatButtonModule } from '@angular/material/button';
 import { NgIf } from '@angular/common';
+import { UserService } from '../../../services/user/user.service';
+import { AuthorizationService } from '../../../services/authorization/authorization.service';
+import { CategoryInputComponent } from '../../UI/input/category-input/category-input.component';
+import { Category, SelectedCategory } from '../../../models/category';
 
 @Component({
   selector: 'app-profile-edit-form',
   standalone: true,
-  imports: [FormInputComponent, ReactiveFormsModule, MatButtonModule, NgIf],
+  imports: [FormInputComponent, ReactiveFormsModule, MatButtonModule, CategoryInputComponent, NgIf],
   templateUrl: './profile-edit-form.component.html',
   styleUrl: './profile-edit-form.component.scss'
 })
 export class ProfileEditFormComponent {
   @Input() profile? : UserProfile;
-  @Output() setIsEditProfile : EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() setIsEditProfile : EventEmitter<UserUpdateProfile | null> 
+    = new EventEmitter<UserUpdateProfile | null>();
+  categories :number[] = []
+  selectedCategories : SelectedCategory[] = [];
+  user? : User;
   editProfileForm! : FormGroup; 
-  constructor(public authService : FormBuilder) 
-  {}
+  constructor(public formBuilder : FormBuilder,
+    public userService : UserService,
+    public authService : AuthorizationService) { }
 
   ngOnInit(): void {
-    this.editProfileForm = this.authService.group({
+    this.editProfileForm = this.formBuilder.group({
       firstName : new FormControl(this.profile?.firstName, [Validators.required]),
       lastName : new FormControl(this.profile?.lastName, [Validators.required]),
       secondName : new FormControl(this.profile?.secondName, [Validators.required]),
       phone : new FormControl(this.profile?.phoneNumber, [Validators.required]),
       email : new FormControl(this.profile?.email, [Validators.required, Validators.email]),
       description : new FormControl(this.profile?.description, [Validators.required, Validators.minLength(10), Validators.maxLength(2000)]),
+  
     });
+
+    this.authService.getUser().subscribe(user => {
+      this.user = user as User;
+    });
+
+    this.selectedCategories = (this.profile as UserProfile).categories.map(category => {
+      return {id: category.id, name: category.name, isSelected: true}
+    });
+  }
+  
+  toggleCategory($event: SelectedCategory) {
+    let category = this.categories.find(x => x == $event.id);
+
+    if (category) {
+      if (!$event.isSelected) {
+        this.categories = this.categories.filter(x => x != $event.id);
+      }
+    } else {
+      if ($event.isSelected) {
+        this.categories.push($event.id); 
+      }      
+    }
   }
 
   onSubmit() {
-    console.log(this.editProfileForm);
-    this.setIsEditProfile.emit(true);
+    if (this.editProfileForm.valid) {
+
+      let profile : UserUpdateProfile = {
+        id : this.user?.id as number,
+        firstName : this.editProfileForm.get('firstName')?.value,
+        secondName : this.editProfileForm.get('secondName')?.value,
+        lastName : this.editProfileForm.get('lastName')?.value,
+        phoneNumber : this.editProfileForm.get('phone')?.value,
+        description : this.editProfileForm.get('description')?.value,
+        categories : this.selectedCategories,
+      };
+
+      this.userService.updateProfile({...profile, categoryIds : this.categories})
+      .then(() => {
+        this.setIsEditProfile.emit(profile);
+      })
+      .catch(error => {
+        console.log(error);
+        this.setIsEditProfile.emit(null);
+      })
+    }
   }
 }
--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserCard } from '../../models/user';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss'
 })
 export class UserCardComponent {
+  @Input() user? : UserCard
 
+  ngOnInit(): void {
+    this.user = {
+      login: 'Artem',
+      id : 0,
+      description : 'some description',
+      isVIP : false,
+      categories : {},
+      avatar : null
+    }
+  }
 }
