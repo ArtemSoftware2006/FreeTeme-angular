a9b14c076a61d615df4b6b0653a80f4b9cd84012
Artem <artem.egorov.art2006@gmail.com>
2024-01-27 21:25:22 +0300

    Изменил Главную страницу

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProfileEditFormComponent } from '../../components/form/profile-edit-form/profile-edit-form.component';
 import { ProposalCreaeteFormComponent } from '../../components/form/proposal-creaete-form/proposal-creaete-form.component';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ProfileEditFormComponent, ProposalCreaeteFormComponent],
+  imports: [ProfileEditFormComponent, ProposalCreaeteFormComponent, MatButtonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
