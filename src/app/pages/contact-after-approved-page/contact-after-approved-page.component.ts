56c2256649e008f591ad261f0043e61f4102d9df
Artem <artem.egorov.art2006@gmail.com>
2024-01-29 23:21:02 +0300

    Создал страницу контактов пользователей

--- src/app/pages/contact-after-approved-page/contact-after-approved-page.component.ts
+++ src/app/pages/contact-after-approved-page/contact-after-approved-page.component.ts
@@ -1,12 +1,40 @@
+import { Subject } from 'rxjs';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserProfile } from '../../models/user';
+import { UserService } from '../../services/user/user.service';
+import { UserContactCardComponent } from '../../components/cards/user-contact-card/user-contact-card.component';
 
 @Component({
   selector: 'app-contact-after-approved-page',
   standalone: true,
-  imports: [],
+  imports: [UserContactCardComponent],
   templateUrl: './contact-after-approved-page.component.html',
   styleUrl: './contact-after-approved-page.component.scss'
 })
 export class ContactAfterApprovedPageComponent {
 
+  employeeId? : number;
+  executorId? : number;
+  employee : Subject<UserProfile> = new Subject<UserProfile>();
+  executor : Subject<UserProfile> = new Subject<UserProfile>();
+
+  constructor(public activatedRoute : ActivatedRoute,
+    public userService : UserService) { }
+
+  ngOnInit(): void {
+    this.employeeId = Number(this.activatedRoute.snapshot.paramMap.get('employeeId'));
+    this.executorId = Number(this.activatedRoute.snapshot.paramMap.get('executorId'));
+
+    this.userService.getUser(this.employeeId)
+    .subscribe((employee) => {
+      this.employee?.next(employee);
+    });
+
+    this.userService.getUser(this.executorId)
+    .subscribe((executor) => {
+      this.executor?.next(executor);
+    });
+
+  }
 }
