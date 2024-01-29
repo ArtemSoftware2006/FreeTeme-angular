4072ee62a52af16f891ab9ecf221d6dea5d2fd8c
Artem <artem.egorov.art2006@gmail.com>
2024-01-29 23:24:38 +0300

    Добавил Бургер меню

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ProfileComponent } from '../profile/profile.component';
 import { LinkComponent } from '../UI/link/link.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { NgClass } from '@angular/common';
+
+
 @Component({
   selector: 'app-main-header',
   standalone: true,
-  imports: [ProfileComponent, LinkComponent],
+  imports: [ProfileComponent, LinkComponent, MatSidenavModule, MatIconModule, MatButtonModule, NgClass],
   templateUrl: './main-header.component.html',
   styleUrl: './main-header.component.scss'
 })
 export class MainHeaderComponent {
+  isOpenMenu = false;
+  isSmallScreen = false;
+  constructor() {
+      this.getScreenSize();
+  }
+  @HostListener('window:resize', ['$event'])
+  getScreenSize() {
+      this.isSmallScreen = window.screen.width < 768;
+  }
 
+  toggleSidenav() {
+    this.isOpenMenu = !this.isOpenMenu;
+    console.log(this.isOpenMenu);
+  }
 }
